0eaa0c9212fc9d3e4411ff1e4e95d1b6dd163182
manuelcasas <128051762+manuelcasas23@users.noreply.github.com>
2024-06-11 18:19:40 +0200

    Actualizar pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,22 +1,28 @@
 input.onButtonPressed(Button.A, function () {
     while (true) {
         if (maqueen.Ultrasonic(PingUnit.Centimeters) < 5) {
+            strip.showColor(neopixel.colors(NeoPixelColors.Red))
             maqueen.motorStop(maqueen.Motors.All)
             maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 25)
             maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CCW, 25)
             basic.pause(1000)
         } else {
+            strip.showColor(neopixel.colors(NeoPixelColors.Green))
             maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 50)
         }
     }
 })
 input.onButtonPressed(Button.B, function () {
     while (true) {
+        strip.showColor(neopixel.colors(NeoPixelColors.Blue))
         maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOn)
         maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOn)
         basic.pause(500)
+        strip.showColor(neopixel.colors(NeoPixelColors.White))
         maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOff)
         maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOff)
         basic.pause(500)
     }
 })
+let strip: neopixel.Strip = null
+strip = neopixel.create(DigitalPin.P15, 4, NeoPixelMode.RGB)
